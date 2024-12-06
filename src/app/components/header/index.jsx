5ef749780ce87294620cea81fef469f14bd062d6
Beyza Keser <beyzaaakeser@gmail.com>
2024-12-06 16:55:17 +0300

    update

--- src/app/components/header/index.jsx
+++ src/app/components/header/index.jsx
@@ -14,6 +14,7 @@ const Header = async () => {
 
       <nav className="flex gap-5">
         <Link href={'/'}>Ana Sayfa</Link>
+        <Link href={'/gallery'}>7 Harika</Link>
         <Link href={'/recipes'}>Tarifler ({data.recipes.length}) </Link>
         <Link href={'/article'}>Makaleler </Link>
         <Link href={'/iletisim'}>İletişim</Link>
---
+++ src/app/gallery/page.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { data } from '../constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+const Gallery = () => {
+  let user = {
+    name: 'beyza',
+    role: 'admin',
+  };
+
+  // admin olmayan kullanıcıları anasayfaya yönlendirelim
+  // server component olduğunda dolayı redirect kullandık
+  if (user.role !== 'admin') {
+    redirect('/');
+  }
+
+  return (
+    <div className="container mx-auto p-5">
+      <h1 className="text-center text-3xl font-bold my-10">
+        The Seven Wonders of the World
+      </h1>
+
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10">
+        {data.map((item) => (
+          <Link key={item.id} href={`/gallery/${item.id}`}>
+            <Image
+              src={item.src}
+              alt={item.name}
+              className="w-full object-cover
+           aspect-square rounded-md"
+              quality={40}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
